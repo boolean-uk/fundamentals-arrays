4395b58dfd97313c9835ff1e1ed37fbb9bb2324c
Olaoluwa Oyebode <olaolumcpaul@gmail.com>
2024-04-18 15:31:03 +0100

    completed exercise

--- src/extension.js
+++ src/extension.js
@@ -8,19 +8,27 @@ const countries = ['Bolivia', 'Jordan', 'Greenland']
 const fruits = ['Apple', 'Orange', 'Pear']
 
 // 1. Add Fred to the names array
+console.log(names.unshift ('Fred'));
 
 // 2. Add 4 to the end of the numbers array
+console.log(numbers.push (4));
 
 // 3. Add Rio to the start of the cities array
+console.log(cities.unshift('Rio'));
 
 // 4. Remove the first colour from the colours array
+console.log(colours.shift());
 
 // 5. Remove the last item from the keys array
+console.log(keys.pop());
 
 // 6. Remove Jordan from the countries array
+console.log(countries.splice(1,1));
 
 // 7. Remove the last fruit from the fruits array and store it in pear below
-const pear = undefined
+const pear = fruits.pop()
+console.log(fruits);
+
 
 // Do not change the code below
 module.exports = {
