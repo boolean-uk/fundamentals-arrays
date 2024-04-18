c0236e86b6001e3698556b4576ae5f05edacc307
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-04-18 14:11:44 +0100

    Completed all assignmemts.

--- src/advanced.js
+++ src/advanced.js
@@ -5,41 +5,41 @@ const numsToMerge = [...nums]
 const arrayToExtend = [...nums]
 
 // 1. Set this variable to the sum of the first and last numbers in the nums array
-const firstPlusLast = (2 + 21);
-console.log(firstPlusLast);
+const firstPlusLast = 2 + 21
+console.log(firstPlusLast)
 
 // 2. set this variable to the index where the number 14 is located in the array
 const indexOfFourteen = 6
 
 // 3. remove the last 3 elements of the array in one single instruction
-const withoutLastThree = nums.splice(0, 6);
-console.log(withoutLastThree);
+const withoutLastThree = nums.splice(0, 6)
+console.log(withoutLastThree)
 
 // 4. Add the number 15 before the number 8 in the array arrayToExtend
 // note: use splice, read the documentation carefully
-arrayToExtend.splice(3,0,15);
-console.log(arrayToExtend);
+arrayToExtend.splice(3, 0, 15)
+console.log(arrayToExtend)
 
 // 5. check if the array includes the number 100
-const includesHundred = arrayToExtend.includes(100);
-console.log(includesHundred);
+const includesHundred = arrayToExtend.includes(100)
+console.log(includesHundred)
 
 // 6. extract the numbers 7, 3, 8 into a new array
-const withSevenThreeEight = [7, 3, 8];
-console.log(withSevenThreeEight);
+const withSevenThreeEight = [7, 3, 8]
+console.log(withSevenThreeEight)
 
 // 7. add the numbers from otherNums to the end of numsToMerge
-const combinedNums = numsToMerge.concat(otherNums);
-console.log(combinedNums);
+const combinedNums = numsToMerge.concat(otherNums)
+console.log(combinedNums)
 
 // 8. create a new array with the cities 'Rome', 'Paris' and 'Barcelona'
-const newCities = ["Rome", "Paris", "Barcelona"];
-console.log(newCities);
+const newCities = ['Rome', 'Paris', 'Barcelona']
+console.log(newCities)
 
 // 9. use the spread operator ... to clone the cities array and add 'Berlin' to the end and 'Sydney' to the start
 // note: read about the spread operator
-const moreCities = ["Sydney", ...newCities, "Berlin"];
-console.log(moreCities);
+const moreCities = ['Sydney', ...newCities, 'Berlin']
+console.log(moreCities)
 
 module.exports = {
   a: firstPlusLast,
--- src/core.js
+++ src/core.js
@@ -4,33 +4,33 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
 const fourthTeacher = 'Phil'
-console.log(fourthTeacher);
+console.log(fourthTeacher)
 
 // 2. Replace the fifth teacher in the teachers array with 'Patrick'
-teachers.splice(4, 1, "Patrick");
-console.log('Replaced fifth teacher:' + teachers);
+teachers.splice(4, 1, 'Patrick')
+console.log('Replaced fifth teacher:' + teachers)
 
 // 3. Remove the last teacher from the array and save them in lastTeacher below
-const lastTeacher = "Shahzad";
-teachers.pop();
-console.log('Removed last teacher:' + teachers);
+const lastTeacher = 'Shahzad'
+teachers.pop()
+console.log('Removed last teacher:' + teachers)
 
 // 4. Remove the first teacher from the array and save them in firstTeacher below
-const firstTeacher = "Nathan";
-teachers.shift();
+const firstTeacher = 'Nathan'
+teachers.shift()
 console.log('Removed first teacher:' + teachers)
 
 // 5. Add a teacher named 'Vanessa' to the end of the teachers array
-teachers.push("Vanessa");
-console.log('New Teacher Added: ' + teachers);
+teachers.push('Vanessa')
+console.log('New Teacher Added: ' + teachers)
 
 // 6. Remove 'Ed' from the teachers array
-teachers.splice(0, 1);
-console.log('Removed Ed: ' + teachers);
+teachers.splice(0, 1)
+console.log('Removed Ed: ' + teachers)
 
 // 7. Add a teacher named 'Sarah' to the beginning of the teachers array
-teachers.unshift("Sarah");
-console.log('Added another: ' + teachers);
+teachers.unshift('Sarah')
+console.log('Added another: ' + teachers)
 
 // Don't change the code below this line
 module.exports = {
