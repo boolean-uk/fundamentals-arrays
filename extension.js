13e4a8c8539bd5f15e1ac538ea6973a61a2a9a03
Max Olofsson <max.olofsson@se.experis.com>
2024-09-20 10:10:53 +0200

    core + extension done

---
+++ extension.js
@@ -0,0 +1,41 @@
+// Do not edit the variables below directly
+const cities = ['London', 'Shanghai', 'New York', 'Delhi', 'Kuala Lumpur']
+const names = []
+const numbers = [1, 2, 3]
+const colours = ['Red', 'Blue', 'Yellow']
+const keys = ['q', 'w', 'e', 'r', 't', 'y']
+const countries = ['Bolivia', 'Jordan', 'Greenland']
+const fruits = ['Apple', 'Orange', 'Pear']
+
+// 1. Add Fred to the names array
+names.push('Fred')
+
+// 2. Add 4 to the end of the numbers array
+numbers.push(4)
+
+// 3. Add Rio to the start of the cities array
+cities.unshift('Rio')
+
+// 4. Remove the first colour from the colours array
+colours.shift()
+
+// 5. Remove the last item from the keys array
+keys.pop()
+
+// 6. Remove Jordan from the countries array
+countries.splice(countries.indexOf('Jordan'), 1)
+
+// 7. Remove the last fruit from the fruits array and store it in pear below
+const pear = fruits.pop()
+
+// Do not change the code below
+module.exports = {
+  a: names,
+  b: numbers,
+  c: cities,
+  d: colours,
+  e: keys,
+  f: countries,
+  g: fruits,
+  h: pear
+}
