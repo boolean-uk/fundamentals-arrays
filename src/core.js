0a1d893413b5b566a01ac86213b6de7333d1bf54
Silje Gjesdal Jacobsen <siljegja@gmail.com>
2024-09-20 11:50:25 +0200

    finished core, extensions, advanced

--- src/core.js
+++ src/core.js
@@ -3,21 +3,34 @@ const teachers = ['Nathan', 'Ed', 'Steve', 'Phil', 'Carlo', 'Lewis', 'Shahzad']
 
 // 1. Using an index on the teachers array, change the value of the
 // fourthTeacher variable below to be the fourth teacher listed in the array
-const fourthTeacher = undefined
+const fourthTeacher = teachers[3]
 
 // 2. Replace the fifth teacher in the teachers array with 'Patrick'
+teachers.splice(4, 1, 'Patrick')
+// teachers[4] = 'Patrick' alternative
+// console.log(teachers)
 
 // 3. Remove the last teacher from the array and save them in lastTeacher below
-const lastTeacher = undefined
+const lastTeacher = teachers.splice(6, 1).toString()
+// console.log(teachers)
+// console.log(lastTeacher)
 
 // 4. Remove the first teacher from the array and save them in firstTeacher below
-const firstTeacher = undefined
+const firstTeacher = teachers.splice(0, 1).toString()
+// console.log(teachers)
+// console.log(firstTeacher)
 
 // 5. Add a teacher named 'Vanessa' to the end of the teachers array
+teachers.push('Vanessa') // add elm to end of array
+// console.log(teachers)
 
 // 6. Remove 'Ed' from the teachers array
+teachers.shift() // removes first elm of array
+// console.log(teachers)
 
 // 7. Add a teacher named 'Sarah' to the beginning of the teachers array
+teachers.unshift('Sarah') // add elm to start of array
+// console.log(teachers)
 
 // Don't change the code below this line
 module.exports = {
